c3a2bf620d8a0adbcdad40e76f770b940dbd1303
Cristhian Sevilla <crissev17@gmail.com>
2023-06-20 13:11:37 -0600

    Sección presentacion agregada a la página principal

---
+++ src/components/proyectos.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+import Image from "next/image"
+
+const Proyectos = () => {
+    return (
+
+       
+
+    )
+}
+
+export default Proyectos
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Layout from "@/components/layout"
 import styles from "../styles/header.module.css"
 import BarraRedes from "@/components/barraRedes"
+import Presentacion from "@/components/presentacion"
 
 export default function Home() {
   return (
@@ -8,12 +9,18 @@ export default function Home() {
       title={'Inicio'}
       description={'Desarrollador Web. Disfruto crear cosas para la web, me apasiona transformar ideas en experiencias digitales sólidas y escalables.'}
     >
+
       <BarraRedes/>
+
       <section className={styles.headerinferior}>
         <div className={styles.headertexto }>
           <h1>Hola soy <span>Cristhian</span></h1>
         </div>
       </section>
+
+      <Presentacion/>
+      
+
 
     </Layout>
   )
