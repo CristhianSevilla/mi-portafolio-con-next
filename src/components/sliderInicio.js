e30a8fbe49c293483e9b65e3de9dbfbe2e1c42cc
Cristhian Sevilla <crissev17@gmail.com>
2023-08-21 21:18:02 -0600

    Background guitarla

--- src/components/sliderInicio.js
+++ src/components/sliderInicio.js
@@ -123,52 +123,6 @@ const SliderProyectos = () => {
                     </div>
                 </div>
                 <div className="carousel-item">
-                    <div className={`${styles.slider_background} ${styles.slider_background_crm} `}>
-                        <div className={`${styles.slider_background_sombra}`}>
-                            <div className="contenedor">
-                                <div className={`${styles.info} texto contenedor`}>
-                                    <div className={styles.slider_texto}>
-                                        <div className="texto">
-                                            <h3>Client Connect</h3>
-                                            <p>Desarrollo Front-End</p>
-                                            <p className={styles.descripcioncorta}>CRM con una funcionalidad completa de CRUD y una conexión HTTP directa a una API. Permite gestionar clientes con facilidad, asegurando que no se dupliquen registros.</p>
-                                            <div className={styles.slider_tecnologias}>
-                                                <ul className={styles.tecnologia}>
-                                                    <li>Vite</li>
-                                                    <li>React</li>
-                                                    <li>TailwinCSS</li>
-                                                    <li>React-Router-Dom</li>
-
-                                                    <li>JavaScript</li>
-                                                    <li>Api-Rest</li>
-                                                </ul>
-                                            </div>
-
-                                            <div className={styles.slider_enlaces}>
-                                                <Link href="/proyectos/client-connect">Leer más</Link>
-
-                                                <Link href="https://github.com/CristhianSevilla/CRM-Clientes" aria-label="GitHub" target="_blank" rel="noreferrer">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github enlace-icon ">
-                                                        <title>Código en GitHub</title>
-                                                        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                                                    </svg>
-                                                </Link>
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className={styles.slider_img}>
-                                        <Link href="/proyectos/client-connect">
-                                            <Image src='/imagenes/projects/crm/principal.jpg' width={1000} height={1000} alt="Desarrollador Web" quality={100} priority />
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div className="carousel-item">
                     <div className={`${styles.slider_background} ${styles.slider_background_barber}`}>
                         <div className={`${styles.slider_background_sombra}`}>
                             <div className="contenedor">
@@ -198,6 +152,15 @@ const SliderProyectos = () => {
                                                         <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                                                     </svg>
                                                 </Link>
+{/* 
+                                                <Link href="http://barber-club.domcloud.io/" aria-label="External Link" className="external" rel="noopener noreferrer" target="_blank">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-external-link">
+                                                        <title>Ver Sitio Web</title>
+                                                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                                                        <polyline points="15 3 21 3 21 9"></polyline>
+                                                        <line x1="10" y1="14" x2="21" y2="3"></line>
+                                                    </svg>
+                                                </Link> */}
                                             </div>
                                         </div>
                                     </div>
@@ -259,6 +222,52 @@ const SliderProyectos = () => {
                                     <div className={styles.slider_img}>
                                         <Link href="/proyectos/trago-trax">
                                             <Image src='/imagenes/projects/trax/principal.jpeg' width={1000} height={1000} alt="Desarrollador Web" quality={100} priority />
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="carousel-item">
+                    <div className={`${styles.slider_background} ${styles.slider_background_crm} `}>
+                        <div className={`${styles.slider_background_sombra}`}>
+                            <div className="contenedor">
+                                <div className={`${styles.info} texto contenedor`}>
+                                    <div className={styles.slider_texto}>
+                                        <div className="texto">
+                                            <h3>Client Connect</h3>
+                                            <p>Desarrollo Front-End</p>
+                                            <p className={styles.descripcioncorta}>CRM con una funcionalidad completa de CRUD y una conexión HTTP directa a una API. Permite gestionar clientes con facilidad, asegurando que no se dupliquen registros.</p>
+                                            <div className={styles.slider_tecnologias}>
+                                                <ul className={styles.tecnologia}>
+                                                    <li>Vite</li>
+                                                    <li>React</li>
+                                                    <li>TailwinCSS</li>
+                                                    <li>React-Router-Dom</li>
+
+                                                    <li>JavaScript</li>
+                                                    <li>Api-Rest</li>
+                                                </ul>
+                                            </div>
+
+                                            <div className={styles.slider_enlaces}>
+                                                <Link href="/proyectos/client-connect">Leer más</Link>
+
+                                                <Link href="https://github.com/CristhianSevilla/CRM-Clientes" aria-label="GitHub" target="_blank" rel="noreferrer">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github enlace-icon ">
+                                                        <title>Código en GitHub</title>
+                                                        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                                                    </svg>
+                                                </Link>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={styles.slider_img}>
+                                        <Link href="/proyectos/client-connect">
+                                            <Image src='/imagenes/projects/crm/principal.jpg' width={1000} height={1000} alt="Desarrollador Web" quality={100} priority />
                                         </Link>
                                     </div>
                                 </div>
