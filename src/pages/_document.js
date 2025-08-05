91ab24f6144495513127f69de6349dbcd808bf61
Cristhian Sevilla <crissev17@gmail.com>
2025-08-05 16:05:51 -0600

    fix: upgrade Next.js to v15.4.5 and fix crossOrigin warnings

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,11 +11,10 @@ return (
   <link href="https://cristhiansevilla.com/imagenes/thumbnail.png" type="image/png" rel="image_src" />
   {/* boostrap */}
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  </link>
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
   {/* Fuentes */}
   <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap"
     rel="stylesheet" />
   {/* Normalize */}
@@ -27,7 +26,7 @@ return (
   <NextScript />
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"></script>
+    crossOrigin="anonymous"></script>
 </body>
 
 </Html>
