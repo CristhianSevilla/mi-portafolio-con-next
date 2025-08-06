3bea4b6f109320c88a4c9f55c5905d762e81e562
Cristhian Sevilla <crissev17@gmail.com>
2025-08-05 19:17:38 -0600

    fix: add Vercel deployment configuration to resolve build error

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  trailingSlash: false,
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
